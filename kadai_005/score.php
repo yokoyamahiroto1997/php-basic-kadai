0e62781611719a57fdf89ab84be2d86fad4d9a7d
yokoyamahiroto1997 <168756657+yokoyamahiroto1997@users.noreply.github.com>
2024-05-03 00:20:23 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lnag="ja">
+
+<body>
+    <p>
+        <?php
+        $score1='80';
+        $score2='60';
+        $score3='55';
+        $score4='40';
+        $score5='100';
+        $score6='25';
+        $score7='80';
+        $score8='90';
+        $score9='30';
+        $score10='60';
+
+        echo $sum=$score1 + $score2 + $score3 + $score4 + $score5 +
+        $score6 + $score7 + $score8 + $score9 + $score10;
+        echo '<br>';
+        echo $sum / 10;
+        ?>
+    </p>
+</body>
+
+</html>
